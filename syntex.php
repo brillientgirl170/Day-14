62fdbbefa6a714816f99abdbcc848d5526224a47
brillientgirl170 <brillientgirl170@gmail.com>
2019-10-14 02:08:57 +0600

    start Php

---
+++ syntex.php
@@ -0,0 +1,82 @@
+<?php
+//    echo '<html>';
+//        echo '<head>';
+//            echo '<title>';
+//            echo 'Home';
+//            echo '</title>';
+//        echo '</head>';
+//        echo '<body>';
+//            echo '<h1>This is a heading</h1>';
+//            echo '<p>This is a paragraph</p>';
+//        echo '</body>';
+//    echo '</html>';
+?>
+<!---->
+<!--    <html>-->
+<!--        <head>-->
+<!--            <title>--><?php //echo 'Home'; ?><!--</title>-->
+<!--        </head>-->
+<!--        <body>-->
+<!--            <h1>--><?php //echo 'This is a heading'; ?><!--</h1>-->
+<!--            <p>--><?php //echo 'This is a paragraph'; ?><!--</p>-->
+<!--            <input type="text" value="--><?php //echo 'Hello world'; ?><!--">-->
+<!--        </body>-->
+<!--    </html>-->
+
+
+<?php
+
+
+  // 3 major rules for creating a variable
+
+  /*      * start with $ sign
+          * A-Z, a-z, 0-9, _
+          * No numbering in first
+   */
+
+ // 3 standard rules for creating a variable
+
+ /*       * small letter
+          * meaning full
+          * readable
+  */
+
+
+//    $firstName = 'Sumi';
+//    $lastName = 'Akter';
+//    echo $firstName.' '.$lastName;
+
+    // type of Operator in Php
+
+ /*       1. Arithmetic operator
+            * binary operator(+. -, *, /, %)
+            * unary operator(++, --, -)
+          2. Assignment operator
+                (=, +=, -=, *=, /=, %=, .=)
+          3. Conditional operator
+                (<, <=, >, >=, ==, !=, ===, !==)
+          4. Logical operator
+                (&&, ||, !)
+ */
+
+    // type of statement
+
+ /*
+          1. single line statement
+          2. conditional statement
+            * if,
+            * if else,
+            * if else if,
+            * switch
+          3. repeated statement
+            * for,
+            * while,
+            * do while,
+            * foreach
+  */
+
+
+ ?>
+
+
+
